970c75382204c9441c1e7a24666a1940398cf597
Cameron <hackwithcameron@gmail.com>
2020-10-06 18:11:33 -0700

    Simple Encryption Tool
    A very simple encryption tool to start working with c++. This was an exercise to practice with Classes.

---
+++ cmake-build-debug/CMakeFiles/EncryptionC__.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EncryptionC__.dir/main.cpp.o"
+  "EncryptionC__"
+  "EncryptionC__.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/EncryptionC__.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,122 @@
+#include <iostream>
+
+// Base Encryption Class
+class EncryptionTool {
+protected:
+    std::string Word;
+public:
+    explicit EncryptionTool(std::string word) {
+        Word = std::move(word);
+    }
+
+    void getWord() {
+        std::cout << "The input word is " << Word << std::endl;
+    }
+};
+
+
+// Letter swap class for Encryption Tool
+class Swap: public EncryptionTool {
+private:
+    int FirstIndex;
+    int SecondIndex;
+
+public:
+    Swap(std::string word, int num1, int num2): EncryptionTool(std::move(word)) {
+        FirstIndex = num1;
+        SecondIndex = num2;
+    }
+
+    void LetterSwap() {
+        char temp;
+        temp = Word[FirstIndex];
+        Word[FirstIndex] = Word[SecondIndex];
+        Word[SecondIndex] = temp;
+
+        std::cout << "the word swapped is " << Word << std::endl;
+    }
+};
+
+
+// Reverse word class for Encryption Tool
+class Reverse: public EncryptionTool {
+public:
+    explicit Reverse(std::string word): EncryptionTool(std::move(word)) {
+    }
+
+    void reverseWord() {
+        std::reverse(Word.begin(), Word.end());
+        std::cout << "the word reversed is " << Word << std::endl;
+
+    }
+};
+
+
+// Rotate class to rotate letters in word
+class Rotate: public EncryptionTool {
+private:
+    int RotateFrom;
+    int NumOfChar;
+
+public:
+    Rotate(std:: string word, int rotateFrom, int numOfChar): EncryptionTool(std::move(word)) {
+        RotateFrom = rotateFrom;
+        NumOfChar = numOfChar;
+    }
+
+    void rotateWord() {
+        std::string temp = Word.substr(RotateFrom, NumOfChar);
+        std::rotate(temp.begin(), temp.begin() + 1, temp.end());
+        Word.replace(RotateFrom, NumOfChar, temp);
+        std:: cout << "Word is now " << Word << std::endl;
+    }
+};
+
+
+// Main function
+int main() {
+    std::string word;
+    std::string againInput;
+    std::string yes[] = {"YES", "yes", "Yes", "Y", "y"};
+    bool run = true;
+    bool again;
+
+    std::cout << "\nWelcome to Encryption Tool 1.0.0"<< std::endl;
+    while (run) {
+        std::cout << "Please enter a word to be encrypted:" << std::endl;
+        std::cin >> word;
+        std::cout << "the original word entered is " << word << std::endl;
+        //Reverse word
+        Reverse reversedWord(word);
+        reversedWord.reverseWord();
+        // Swap word
+        Swap inputWord(word, 1, 4);
+        inputWord.LetterSwap();
+        //Rotate word
+        Rotate rotateWord(word, 1, 3);
+        rotateWord.rotateWord();
+
+        run = false;
+//
+//        std::cout << "Would you like to enter another word?" << std::endl;
+//        std::cin >> againInput;
+//
+//        for (int i = 0; i < yes->length(); i++) {
+//            int compairision = againInput.compare(yes[i]);
+//            if (compairision != 0) {again = true;}
+//            else {again = false;}
+//
+//        }
+//
+//        if (again) {
+//            continue;
+//        }
+//        else {
+//            std::cout << "Thank you for using Encryption Tool!" << std::endl;
+//            run = false;
+//        }
+    }
+}
+
+
+
